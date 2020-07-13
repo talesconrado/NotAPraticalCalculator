ab5b3a79ed0404f67907e275e7c057c1dca9e37e
Tales Conrado <talesmateuscc@gmail.com>
2020-07-12 22:34:01 -0300

    Initial Commit

---
+++ NotAGoodCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotAGoodCalculator
+//
+//  Created by Tales Conrado on 12/07/20.
+//  Copyright © 2020 Tales Conrado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
